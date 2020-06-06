d511fb343ec3bbc64376f9da63c6d99e851592cc
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-07 02:32:51 +0530

    Fixed #35, photoviewer statusbar

--- lib/screens/photoViewer.dart
+++ lib/screens/photoViewer.dart
@@ -1,17 +1,32 @@
 import 'dart:io';
+import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:flutter/material.dart';
 
-class PhotoViewer extends StatelessWidget {
-  final File image;
+class PhotoViewer extends StatefulWidget {
+  File image;
   PhotoViewer({@required this.image});
   @override
+  _PhotoViewerState createState() => _PhotoViewerState();
+}
+
+class _PhotoViewerState extends State<PhotoViewer> {
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIOverlays(
+        [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([]);
     return Container(
-        child: PhotoView(
-      imageProvider: FileImage(image),
-      heroAttributes: PhotoViewHeroAttributes(tag: "image"),
-      minScale: .5,
-    ));
+      child: PhotoView(
+        imageProvider: FileImage(widget.image),
+        heroAttributes: PhotoViewHeroAttributes(tag: "image"),
+        minScale: .35,
+      ),
+    );
   }
 }
