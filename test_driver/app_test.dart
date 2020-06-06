6c5407ad83413f8bb1c116d5fbdb1a2af082df8e
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-06 20:28:59 +0530

    Added more tests

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -8,32 +8,70 @@ void main() {
     // be the same as the Strings we used for the Keys in step 1.
     final devName = find.byValueKey('DevName');
     final buttonFinder = find.byType('AboutButton');
+    final backButtonFinder = find.byValueKey('BackButton');
+    final newNoteButtonFinder = find.byValueKey('NewNote');
+    final bodyTextFieldFinder = find.byValueKey('BodyText');
+    final doneButtonFinder = find.byValueKey('Done');
 
     FlutterDriver driver;
-
-    // Connect to the Flutter driver before running any tests.
     setUpAll(() async {
       driver = await FlutterDriver.connect();
     });
 
-    // Close the connection to the driver after the tests have completed.
     tearDownAll(() async {
       if (driver != null) {
         driver.close();
       }
     });
 
-    // test('starts at 0', () async {
-    //   // Use the `driver.getText` method to verify the counter starts at 0.
-    //   expect(await driver.getText(counterTextFinder), "0");
-    // });
-
     test('taps about button', () async {
-      // First, tap the button.
       await driver.tap(buttonFinder);
 
-      // Then, verify the counter text is incremented by 1.
       expect(await driver.getText(devName), "Hash Studios");
+    });
+
+    test('taps back button', () async {
+      await driver.tap(backButtonFinder);
+    });
+
+    test('taps new note button', () async {
+      await driver.tap(newNoteButtonFinder);
+    });
+
+    test('body text entered', () async {
+      await driver.tap(bodyTextFieldFinder);
+      await driver.enterText('New Note');
+
+      expect(await driver.getText(bodyTextFieldFinder), "New Note");
+    });
+
+    test('taps done button', () async {
+      await driver.tap(doneButtonFinder);
+    });
+
+    test('taps new note button', () async {
+      await driver.tap(newNoteButtonFinder);
+    });
+
+    test('body text entered', () async {
+      await driver.tap(bodyTextFieldFinder);
+      await driver.enterText('Another Note');
+
+      expect(await driver.getText(bodyTextFieldFinder), "Another Note");
+    });
+
+    test('taps done button', () async {
+      await driver.tap(doneButtonFinder);
+    });
+
+    test('taps about button', () async {
+      await driver.tap(buttonFinder);
+
+      expect(await driver.getText(devName), "Hash Studios");
+    });
+
+    test('taps back button', () async {
+      await driver.tap(backButtonFinder);
     });
   });
 }
